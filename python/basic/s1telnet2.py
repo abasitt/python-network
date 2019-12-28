3d7a7a418462b95a71853f01ea5fb25024831e44
abasitt <a.basit@hotmail.com>
2019-12-28 08:40:36 +0800

    first upload from local

---
+++ python/basic/s1telnet2.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3.6
+
+import getpass
+import telnetlib
+
+HOST = "192.168.122.28"
+user = input("Enter your username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"en\n")
+tn.write(b"cisco\n")
+tn.write(b"conf t\n")
+tn.write(b"vlan 10\n")
+tn.write(b"name python_10\n")
+tn.write(b"end\n")
+tn.write(b"write\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
---
+++ python/basic/s1telnet3.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3.6
+
+import getpass
+import telnetlib
+
+HOST = "192.168.122.28"
+user = input("Enter your username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"en\n")
+tn.write(b"cisco\n")
+tn.write(b"conf t\n")
+
+
+for n in range (5,15):
+	vlan = "vlan " + str(n)
+	name = "name Python_" + str(n)
+	tn.write(vlan.encode('ascii') + b"\n")
+	tn.write(name.encode('ascii') + b"\n")
+
+
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
