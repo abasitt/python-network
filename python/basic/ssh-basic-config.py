c77f8e61a284231ae7efcaf8adff3d782482efe0
abasitt <a.basit@hotmail.com>
2019-12-29 17:52:28 +0800

    ssh for multiple devices with vlan configs

--- python/basic/ssh-basic-config.py
+++ python/basic/ssh-basic-config.py
@@ -2,7 +2,7 @@
 
 from netmiko import ConnectHandler
 
-cisco_iosv = {
+cisco_iosv1 = {
     'device_type': 'cisco_ios',
     'host':   '192.168.122.31',
     'username': 'admin',
@@ -11,20 +11,30 @@ cisco_iosv = {
     #'secret': 'secret',     # optional, defaults to ''
 }
 
-net_connect = ConnectHandler(**cisco_iosv)
+cisco_iosv2 = {
+    'device_type': 'cisco_ios',
+    'host':   '192.168.122.32',
+    'username': 'admin',
+    'password': 'cisco',
+    #'port' : 8022,          # optional, defaults to 22
+    #'secret': 'secret',     # optional, defaults to ''
+}
 
+cisco_iosv3 = {
+    'device_type': 'cisco_ios',
+    'host':   '192.168.122.33',
+    'username': 'admin',
+    'password': 'cisco',
+    #'port' : 8022,          # optional, defaults to 22
+    #'secret': 'secret',     # optional, defaults to ''
+}
 
-config_commands = ["interface loop1","ip add 1.1.1.1 255.255.255.0"]
+device_list= [cisco_iosv1,cisco_iosv2,cisco_iosv3]
 
-output = net_connect.send_config_set(config_commands)
-
-for n in range (21,31):
-    print ("configuring vlan " + str(n) )
-    config_commands = ["vlan " + str(n), "name pythonvlan_" + str(n)]
-    output = net_connect.send_config_set(config_commands)
-
-output = net_connect.send_command('show ip int brief')
-print(output)
-
-output = net_connect.send_command('show vlan brief')
-print(output)
+for device in device_list:
+    net_connect = ConnectHandler(**device)
+    for n in range (21,24):
+        print ("configuring vlan " + str(n) )
+        config_commands = ["vlan " + str(n), "name pythonvlan_" + str(n)]
+        output = net_connect.send_config_set(config_commands)
+        print(output)
