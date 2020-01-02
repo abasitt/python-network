42414473b82046dff4ee65c9d6f8b16ce384b5a9
abasitt <a.basit@hotmail.com>
2020-01-02 21:42:29 +0800

    Napalm BGP neigbhor retrieve

--- python/basic/network-config-napalm.py
+++ python/basic/network-config-napalm.py
@@ -6,11 +6,5 @@ driver = get_network_driver('ios')
 ios_sw = driver ('192.168.122.31', 'admin', 'cisco')
 ios_sw.open()
 
-ios_output = ios_sw.get_facts()
+ios_output = ios_sw.get_bgp_neighbors()
 print (json.dumps(ios_output, indent=4))
-
-ios_output = ios_sw.get_interfaces()
-print (json.dumps(ios_output, indent=4))
-
-ios_output = ios_sw.get_interfaces_counters()
-print (json.dumps(ios_output,sort_keys=True, indent=4))
